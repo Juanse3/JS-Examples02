f245162a11c67d8966f5ee9c5e744f7dffff210e
JuanM <juanse3@gmail.com>
2022-12-05 20:45:40 -0300

    Se seleccionan elementos a evaluar

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Me aseguro que la pagina haya sido cargada
+document.addEventListener('DOMContentLoaded',function(){
+
+    //selecciono datos
+    const inputEmail = document.querySelector('#email');
+    const inputLabel = document.querySelector('#asunto');
+    const inputMsg = document.querySelector('#mensaje');
+
+    console.log(inputEmail);
+    console.log(inputLabel);
+    console.log(inputMsg);
+});
