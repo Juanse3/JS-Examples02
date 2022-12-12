8f7f2637ab732373d57387a53bab685909898504
JuanM <juanse3@gmail.com>
2022-12-12 20:47:02 -0300

    Se agrega mensaje de error de campo vacio

--- js/app.js
+++ js/app.js
@@ -5,8 +5,96 @@ document.addEventListener('DOMContentLoaded',function(){
     const inputEmail = document.querySelector('#email');
     const inputLabel = document.querySelector('#asunto');
     const inputMsg = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
-    console.log(inputEmail);
-    console.log(inputLabel);
-    console.log(inputMsg);
+    //asigno eventos
+
+    inputEmail.addEventListener('blur',validate_data);
+    inputLabel.addEventListener('blur',validate_data);
+    inputMsg.addEventListener('blur',validate_data);
+
+    function validate_data(e){
+        const evento = e.target.value; 
+        if(evento.trim() === ''){  //Le aplico trim para quitar espacios en blanco al inicio y al final
+            show_error();
+        }else{
+            valid_data(evento);
+        }
+    }
+
+    function show_error(){
+        console.log('Error');
+        //genero el HTML del error
+        const warning_msg = document.createElement('p');
+        warning_msg.textContent = 'Campo Vacio .... '
+        warning_msg.classList.add('bg-red-600','text-white','p-2','text-center'); //agrego una clase de tailwind
+        
+        
+        console.log(warning_msg);
+
+        //Inyecto HTML
+        formulario.appendChild(warning_msg);//agrega un hijo al final
+        
+        
+        //con innerHTML
+        //formulario.innerHTML = warning_msg.innerHTML; //reemplaza todo el contenido por el html generado
+
+    }
+
+    function valid_data(evt){
+        console.log(evt);
+    }
+
+
+
+
+    ///Verifico que el campo no esté vacio
+/*     inputEmail.addEventListener('blur',print_data);
+    inputLabel.addEventListener('blur',print_data);
+    inputMsg.addEventListener('blur',print_data);
+
+    function print_data(e){
+        const evento = e.target.value; 
+        console.log(evento.length);
+        if(evento.trim() === ''){  //Le aplico trim para quitar espacios en blanco al inicio y al final
+            console.log('Campo Vacio');
+        }else{
+            console.log('desde el if');
+            console.log(evento.trim().length);
+            console.log(evento);
+        }
+    } */
+
+
+//Solo reutilizamos la funcion
+/*     inputEmail.addEventListener('blur',print_data);
+    inputLabel.addEventListener('blur',print_data);
+    inputMsg.addEventListener('blur',print_data);
+
+    function print_data(e){
+        console.log('Desde funcion');
+        console.log(e.target.value)
+    } */
+
+   ///Mucho codigo repetido que se elimina utilizando funciones  
+/*     inputEmail.addEventListener('blur',function(e){
+        console.log("input_Email");
+        console.log(e.target.value);
+    });
+    inputLabel.addEventListener('blur',function(e){
+        console.log("input_Label");
+        console.log(e.target.value);
+    });
+    inputMsg.addEventListener('blur',function(e){
+        console.log("input_Msg");
+        console.log(e.target.value);
+    }); */
+    // Llamada en tiempo real de lo ingresado al campo
+/*     inputEmail.addEventListener('input',function(e){
+        console.log("INPUT EVENT");
+        console.log(e.target.value);
+        console.log(e);
+    }); */
+
 });
+
